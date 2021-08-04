e9b8679ff24fb8b4ae57bda522b40a7f9dc6c072
Thomas Jochmann <thomas@jochmann.info>
2021-08-04 11:47:23 +0200

    dummy file

---
+++ foo.py
@@ -0,0 +1,21 @@
+# %% Import
+
+import pandas as pd
+
+
+# %% Generate some dummy data
+
+df_qsm = pd.DataFrame({"rois": ['gm', 'wm', 'csf'], "chi":[3.2, 2.7, 7.1]})
+df_dq = pd.DataFrame({"rois": ['gm', 'wm', 'csf'], "chi":[3.1, 2.2, 5.2]})
+
+
+# %% Show data
+
+df_dq
+
+
+# %% Export data
+
+df_qsm.to_excel("qsm.xls")
+
+df_dq.to_excel("dq.xls")
